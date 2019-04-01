4056818d1c3aee5dd70847ea618c8ce325edf14b
danielgeerts7 <daniel.geerts@live.nl>
2019-04-01 21:21:49 +0200

    Changed layout of Popup, and more images

--- project2.3/src/View/Popup.java
+++ project2.3/src/View/Popup.java
@@ -4,12 +4,14 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 /**
@@ -22,13 +24,17 @@ import javafx.stage.Stage;
 public final class Popup {
 
 	public enum Type {
-		OK, YESNO, DEBUG, WIN, LOSE, DRAW
+		DEBUG, OK, YESNO, WIN, LOSE, DRAW
 	};
 
-	private int width = 400;
-	private int height = 200;
-	private String img_errPath = "File:img/error_icon.gif";
-	private String img_debugPath = "File:img/debug_icon.gif";
+	private int width = 425;
+	private int height = 175;
+	private String img_okPath = "File:img/OK.png";
+	private String img_debugPath = "File:img/DEBUG.png";
+	private String img_YesNoPath = "File:img/YESNO.png";
+	private String img_winPath = "File:img/WIN.png";
+	private String img_losePath = "File:img/LOSE.png";
+	private String img_drawPath = "File:img/DRAW.png";
 
 	private static Popup instance = null;
 
@@ -40,7 +46,7 @@ public final class Popup {
 	}
 
 	private Popup() {
-		
+
 	}
 
 	public void newPopup(String text, Type type) {
@@ -52,21 +58,24 @@ public final class Popup {
 			@Override
 			public void run() {
 				final Stage newStage = new Stage();
-				GridPane pane = new GridPane();
-				pane.setAlignment(Pos.CENTER);
+				newStage.setTitle("Popup: " + type.toString().toLowerCase());
+				Pane pane = new Pane();
 				pane.setMinSize(width, height);
-				pane.setVgap(10);
-				pane.setHgap(10);
 
-				Label MESSAGE = new Label(text);
-				pane.add(MESSAGE, 1, 0);
+				Label message = new Label(text);
+				message.setAlignment(Pos.CENTER);
+				moveItem(message, width*.3, height*0.25);
+				pane.getChildren().add(message);
+				
+				String image = "";
 
 				switch (type) {
 				case OK:
-					ImageView error = getImageView(img_errPath);
-					pane.add(error, 0, 0);
+					image = img_okPath;
 					Button btn_ok = new Button("OK");
-					pane.add(btn_ok, 1, 1);
+					btn_ok.setAlignment(Pos.CENTER);
+					moveItem(btn_ok, width*.5, height*.7);
+					pane.getChildren().add(btn_ok);
 					btn_ok.setOnAction(new EventHandler<ActionEvent>() {
 						@Override
 						public void handle(ActionEvent e) {
@@ -75,10 +84,15 @@ public final class Popup {
 					});
 					break;
 				case YESNO:
+					image = img_YesNoPath;
 					Button btn_yes = new Button("YES");
 					Button btn_no = new Button("NO");
-					pane.add(btn_yes, 0, 1);
-					pane.add(btn_no, 1, 1);
+					pane.getChildren().add(btn_yes);
+					pane.getChildren().add(btn_no);
+					btn_yes.setAlignment(Pos.CENTER);
+					btn_no.setAlignment(Pos.CENTER);
+					moveItem(btn_yes, width*.6, height*0.7);
+					moveItem(btn_no, width*.4, height*0.7);
 
 					btn_yes.setOnAction(new EventHandler<ActionEvent>() {
 						@Override
@@ -101,10 +115,10 @@ public final class Popup {
 					});
 					break;
 				case DEBUG:
-					ImageView debug = getImageView(img_debugPath);
-					pane.add(debug, 0, 0);
+					image = img_debugPath;
 					Button btn_debug = new Button("OK");
-					pane.add(btn_debug, 1, 1);
+					moveItem(btn_debug, width*.5, height*.7);
+					pane.getChildren().add(btn_debug);
 					btn_debug.setOnAction(new EventHandler<ActionEvent>() {
 						@Override
 						public void handle(ActionEvent e) {
@@ -113,12 +127,22 @@ public final class Popup {
 					});
 					break;
 				case WIN:
+					image = img_winPath;
+
 					break;
 				case LOSE:
+					image = img_losePath;
+
 					break;
 				case DRAW:
+					image = img_drawPath;
+
 					break;
 				}
+				
+				ImageView error = getImageView(image);
+				moveItem(error, width*.02, height*.2);
+				pane.getChildren().add(error);
 
 				Scene stageScene = new Scene(pane, width, height);
 				newStage.setScene(stageScene);
@@ -130,10 +154,15 @@ public final class Popup {
 	private ImageView getImageView(String path) {
 		Image image = new Image(path);
 		ImageView pic = new ImageView();
-		pic.setFitWidth(50);
-		pic.setFitHeight(50);
+		pic.setFitWidth(100);
+		pic.setFitHeight(100);
 		pic.setImage(image);
 		return pic;
+	}
+	
+	private void moveItem(Node n, double x, double y) {
+		n.setTranslateX(x);
+		n.setTranslateY(y);
 	}
 
 	public interface PopupYesNo {
