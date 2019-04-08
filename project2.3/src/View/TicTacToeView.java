8f2d602ec6c39cbcebb93443aa94fe2f35d30e10
Joppert123 <44192052+Joppert123@users.noreply.github.com>
2019-04-08 15:13:32 +0200

    TicTacToe Start

---
+++ project2.3/src/View/TicTacToeView.java
@@ -0,0 +1,13 @@
+package View;
+
+import Model.Config;
+import javafx.geometry.Pos;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+
+public class TicTacToeView extends GameView {
+	
+	private GridPane pane = null;
+
+}
